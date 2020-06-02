d98c8bd54a414bf2dfdb8a1b803851b8594c86bb
Alexander Augusto <alexaasf_10@hotmail.com>
2020-06-02 09:15:15 -0300

    new changes

---
+++ dist/precache-manifest.26359b2c6caddc6f7dc5e2c81d37713d.js
@@ -0,0 +1,170 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e8752616d88ff98fd04152d685f7eec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16dca234f15e23d6b12f",
+    "url": "/static/css/2.8f60120a.chunk.css"
+  },
+  {
+    "revision": "fc0a237e026845ad29e6",
+    "url": "/static/css/main.9e506ca1.chunk.css"
+  },
+  {
+    "revision": "16dca234f15e23d6b12f",
+    "url": "/static/js/2.5021527e.chunk.js"
+  },
+  {
+    "revision": "299cba5f6c747cbc295097594263041b",
+    "url": "/static/js/2.5021527e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc0a237e026845ad29e6",
+    "url": "/static/js/main.8e3508fc.chunk.js"
+  },
+  {
+    "revision": "6ab68a74d4a1214dc253",
+    "url": "/static/js/runtime-main.1e88ae78.js"
+  },
+  {
+    "revision": "632d6e826df3bffae4b044f957f07ad9",
+    "url": "/static/media/alexander_augusto.632d6e82.jpg"
+  },
+  {
+    "revision": "c34ae324dde9974e297c1f8d1e1063bc",
+    "url": "/static/media/aurem.c34ae324.PNG"
+  },
+  {
+    "revision": "01899307116a937c66a0eb4b3927b631",
+    "url": "/static/media/blob.01899307.png"
+  },
+  {
+    "revision": "9212eccb6f07b311e2a9d064ebd5a5d0",
+    "url": "/static/media/central_modas.9212eccb.png"
+  },
+  {
+    "revision": "8c3942f68e2bed1cb16076efb21661f6",
+    "url": "/static/media/central_modas_2.8c3942f6.PNG"
+  },
+  {
+    "revision": "8d581ffb03c56a0106abc6b330b449d5",
+    "url": "/static/media/cercuri.8d581ffb.png"
+  },
+  {
+    "revision": "c3189fd0b0ab4d2e1a05fd6acc8f1279",
+    "url": "/static/media/cv.c3189fd0.pdf"
+  },
+  {
+    "revision": "b81dabc0bc6ca52fce921e3a928d5e0f",
+    "url": "/static/media/dots.b81dabc0.png"
+  },
+  {
+    "revision": "78c8cfa64861b305038f66baf49e9077",
+    "url": "/static/media/fiscoserv.78c8cfa6.PNG"
+  },
+  {
+    "revision": "b942b85f8db5906ec7173969ab8ee783",
+    "url": "/static/media/internship_report.b942b85f.pdf"
+  },
+  {
+    "revision": "4956192ec6e84cffe9ae78621c2f4a8f",
+    "url": "/static/media/my_pets.4956192e.apk"
+  },
+  {
+    "revision": "6ff987607fba3f32fe358bbdf61727ca",
+    "url": "/static/media/my_pets.6ff98760.jpg"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/static/media/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/static/media/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/static/media/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/static/media/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "e3bf2140e3df2c5e41303be3341dddaa",
+    "url": "/static/media/otavio_augusto.e3bf2140.jpg"
+  },
+  {
+    "revision": "43ff45c5dc1f65726b86fc75ea6cabc0",
+    "url": "/static/media/path2.43ff45c5.png"
+  },
+  {
+    "revision": "a70167ea1491987c50049961a9e2e45f",
+    "url": "/static/media/path3.a70167ea.png"
+  },
+  {
+    "revision": "e37326c7e0fd9826c1a35b231340f58b",
+    "url": "/static/media/path4.e37326c7.png"
+  },
+  {
+    "revision": "ceb241f05718fb1311f8810eea959803",
+    "url": "/static/media/path5.ceb241f0.png"
+  },
+  {
+    "revision": "03ea2a0ad05689cebb5a9314a53b8966",
+    "url": "/static/media/patrat.03ea2a0a.png"
+  },
+  {
+    "revision": "4001613ef5b13ac31eba9a1bc8b3a0ce",
+    "url": "/static/media/paulo_junior.4001613e.jpg"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/static/media/revicons.2feb69cc.eot"
+  },
+  {
+    "revision": "98742a820d2986e78ae284d57deae457",
+    "url": "/static/media/ripp.98742a82.png"
+  },
+  {
+    "revision": "04bc04fbf6aae4e25e97cd9d4a794521",
+    "url": "/static/media/society.04bc04fb.apk"
+  },
+  {
+    "revision": "ca25707397fcc982d7dc1f82555f09a0",
+    "url": "/static/media/square1.ca257073.png"
+  },
+  {
+    "revision": "6619b9792d7d108bb589b51bbd560278",
+    "url": "/static/media/square2.6619b979.png"
+  },
+  {
+    "revision": "23fa76237f0066f4ffd8f20e77449887",
+    "url": "/static/media/square3.23fa7623.png"
+  },
+  {
+    "revision": "8fdf9ee20db40e5d1cf778bf99486cb7",
+    "url": "/static/media/square4.8fdf9ee2.png"
+  },
+  {
+    "revision": "ff004a0e5de54ba9dbd8583fa7e1b3f5",
+    "url": "/static/media/square5.ff004a0e.png"
+  },
+  {
+    "revision": "faae7463837538bf71fd654a391e3064",
+    "url": "/static/media/supervisor.faae7463.apk"
+  },
+  {
+    "revision": "11fa5f73cb719f0f1a38e02e47bdc843",
+    "url": "/static/media/waves.11fa5f73.png"
+  }
+]);
